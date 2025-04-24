c2b754da05cdf6e1d69f0e25ab525bf81e60d36c
Maureen Fox <maureenfox2022@gmail.com>
2025-04-24 10:53:45 -0400

    Initial Commit

---
+++ Budget-Buddy/Budget_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_BuddyApp.swift
+//  Budget-Buddy
+//
+//  Created by Maureen Fox on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Budget_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
